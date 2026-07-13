c9b7b373adc1fe8fabcb63a86845c837a4d1062e
kharxhini <kharxhini6@gmail.com>
2026-07-13 10:32:12 +0530

    Update Vowel.java

--- Vowel.java
+++ Vowel.java
@@ -1,10 +1,29 @@
 import java.util.Scanner;
+
 public class Vowel {
-    public static void main(String[] args){
+    public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter a String:");
+
+        System.out.print("Enter a String: ");
         String name = sc.nextLine();
-        if(name)
+
+        int count = 0;
+
+        for (int i = 0; i < name.length(); i++) {
+
+            char ch = name.charAt(i);
+
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' ||
+                ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U') {
+
+                count++;
+                System.out.println(name.charAt(i));
+            }
+        }
+
+        System.out.println("Number of vowels = " + count);
+
+        sc.close();
     }
-    
 }
