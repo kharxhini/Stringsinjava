ec15186296bf67a52fa0092a121b979f4cc3b4a4
kharxhini <kharxhini6@gmail.com>
2026-07-16 10:38:27 +0530

    string

--- Reapt.java
+++ Reapt.java
@@ -1,3 +1,29 @@
+import java.util.Scanner;
 public class Reapt {
-    
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a String:");
+        String str = sc.nextLine();
+        for(int i =0; i<str.length();i++){
+            char ch = str.charAt(i);
+              boolean duplicate = false;
+
+            for(int j=0; j<i;j++){
+                if (str.charAt(i) == str.charAt(j)){
+                  duplicate = true;
+                  break;
+
+                }
+
+                
+                 if(!duplicate){
+                System.out.print(ch);
+            }
+               
+            }
+           
+        }
+        sc.close();
+    }
+
 }
